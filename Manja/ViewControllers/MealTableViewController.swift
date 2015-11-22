3381c21bc6530a1dec1d9c0c87a45cf7a9e63e56
Faustino Dabraio da Silva <faustino.silva@gmail.com>
2015-11-22 03:42:33 +0000

    Automatically focus first editable cell in Meal view and show keyboard

--- Manja/ViewControllers/MealTableViewController.swift
+++ Manja/ViewControllers/MealTableViewController.swift
@@ -42,12 +42,17 @@ class MealTableViewController: UITableViewController, UITextFieldDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        switch editMode {
+        case .Add:
+            (tableView.cellForRowAtIndexPath(NSIndexPath(forRow: 4, inSection: 0)) as! EditableTextTableViewCell).txtField.becomeFirstResponder()
+        case .New:
+            (tableView.cellForRowAtIndexPath(NSIndexPath(forRow: 0, inSection: 0)) as! EditableTextTableViewCell).txtField.becomeFirstResponder()
+        case .Edit:
+            (tableView.cellForRowAtIndexPath(NSIndexPath(forRow: 1, inSection: 0)) as! EditableTextTableViewCell).txtField.becomeFirstResponder()
+        }
     }
 
     override func didReceiveMemoryWarning() {
